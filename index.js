fb65a8b5baa868614d33cf26b065248ca0d2a166
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-08 19:07:21 -0700

    Create POST endpoint

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
-      res.json(users);
+      return res.json(users);
     }).catch(err => {
-      res.status(500).json({ error: "There was an error while saving the user to the database" });
+      return res.status(500).json({ error: "There was an error while saving the user to the database" });
     })
 })
 
@@ -19,12 +21,32 @@ server.get('/api/users/:id', (req, res) => {
   db.findById(req.params.id)
     .then(user => {
       if (user) {
-        res.json(user);
+        return res.json(user);
       } else {
-        res.status(404).json({ message: "The user with the specified ID does not exist." });
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
       }
     }).catch(err => {
-      res.status(500).send({ error: "The user information could not be retrieved." });
+      return res.status(500).send({ error: "The user information could not be retrieved." });
+    })
+})
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+
+  if (!newUser.name || !newUser.bio) {
+    return res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+  }
+
+  db.insert(newUser)
+    .then(newId => {
+      db.findById(newId.id)
+        .then(userData => {
+          return res.status(201).json(userData);
+        }).catch(err => {
+          return res.send(err);
+        })
+    }).catch(err => {
+      return res.status(500).json({ error: "There was an error while saving the user to the database" });
     })
 })
 
