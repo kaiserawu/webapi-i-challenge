4c32d2b02cd928d85f7aea2e9c5b53a816be0f91
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-08 19:20:09 -0700

    Create DELETE endpoint

--- index.js
+++ index.js
@@ -50,6 +50,26 @@ server.post('/api/users', (req, res) => {
     })
 })
 
+server.delete('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+
+  db.findById(userId)
+    .then(user => {
+      if (user) {
+        db.remove(userId)
+          .then(() => {
+            return res.json(user);
+          }).catch(err => {
+            return res.status(500).json({ error: "The user could not be removed" });
+          })
+      } else {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+    }).catch(err => {
+      return res.send(err);
+    })
+})
+
 server.listen(9090, () => {
   console.log('Listening on port 9090');
 })
